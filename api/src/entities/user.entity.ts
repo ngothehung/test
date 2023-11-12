1c3f2718dbb4aee0af1083817e7d2738fea106c6
ngothehung <ngothehung2110@gmail.com>
2023-11-13 01:24:31 +0700

    slide && user

---
+++ api/src/entities/user.entity.ts
@@ -0,0 +1,44 @@
+import { Column, Entity, JoinColumn, OneToMany, PrimaryGeneratedColumn, Unique } from "typeorm";
+
+@Unique(["email"])
+@Unique(["username"])
+@Unique(["phone"])
+@Entity('users')
+class User {
+    @PrimaryGeneratedColumn()
+    public id: number;
+
+    @Column()
+    public name: string;
+
+    @Column()
+    public username: string;
+
+    @Column()
+    public email: string;
+
+    @Column()
+    public password: string;
+
+    @Column()
+    public phone: string;
+
+    @Column()
+    public address: string;
+
+    @Column()
+    public avatar: number;
+
+    @Column()
+    public refresh_token: string;
+
+    @Column()
+    public created_at: Date;
+
+    @Column()
+    public updated_at: Date;
+
+   
+}
+
+export default User;
